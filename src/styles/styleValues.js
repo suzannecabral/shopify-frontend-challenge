10e19485f7bd1aa728cbd551d4f46254964c063e
Suzanne Cabral <suzannecabral@users.noreply.github.com>
2021-05-06 15:59:27 -0700

    Add style constants

---
+++ src/styles/styleValues.js
@@ -0,0 +1,20 @@
+export const COLORS = {
+
+    // accent: light gold
+    accentMain:'#d9bb56',
+    accentLight:'#ECDDAA',
+    accentDark:'#827033',
+    accentDeep:'#564A22',
+
+    // opposite: cornflower
+    accentComplementary:'#5674d9',
+    
+    // neutral: blue-tinted grays
+    neutralWhite:'#f9fafd',
+    neutralOffWhite:'#F1F3FB',
+    neutralMid:'#e3f2fd',
+    neutralDark:'#464950',
+    neutralOffBlack:'#232428',
+    neutralBlack:'#151518',
+
+}
