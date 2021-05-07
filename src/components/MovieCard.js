0d4bd6bdb2d4c2c33527179fbc084d3a52d6b648
Suzanne Cabral <suzannecabral@users.noreply.github.com>
2021-05-07 14:07:26 -0700

    Add repeating movieCard component and dummy data

---
+++ src/components/MovieCard.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import styled from 'styled-components';
+import COLORS from '../styles/styleValues';
+
+const StyledMovieCard = styled.div`
+    background-color:${COLORS.neutralLight};
+    color:${COLORS.neutralBlack};
+    border: 1px solid red;
+    padding:15px 20px;
+    margin:5px;
+`;
+
+const MovieCard = props => {
+    const { Title, Year, Genre } = props.movie;
+    console.log("Movie title:", Title);
+    return(
+        <StyledMovieCard>
+            <p>Title: {Title}</p>
+            <p>Year: {Year}</p>
+            <p>Genre: {Genre}</p>
+        </StyledMovieCard>
+    );
+}
+
+export default MovieCard;
--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -2,15 +2,26 @@ import React from 'react';
 import styled from 'styled-components';
 import COLORS from '../styles/styleValues';
 
+import MovieCard from './MovieCard';
+import data from '../dummy';
+
+
 const StyledSearchResults = styled.div`
-    background-color:${COLORS.neutralLight};
-    color:${COLORS.neutralBlack};
+background-color:${COLORS.neutralLight};
+color:${COLORS.neutralBlack};
+border: 1px solid blue;
 `;
 
 const SearchResults = props => {
+    const movies = data;
     return(
         <StyledSearchResults>
-            <p>SearchResults</p>
+            <p>Search Results:</p>
+            {
+                movies.map((movie, i) => (
+                    <MovieCard movie={movie} key={i} />
+                ))
+            }
         </StyledSearchResults>
     );
 }
