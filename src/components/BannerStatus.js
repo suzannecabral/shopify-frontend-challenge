004349f2de0788dbdea93672d7cd34dc02798843
Suzanne Cabral <suzannecabral@users.noreply.github.com>
2021-05-06 18:10:05 -0700

    Style Banner

--- src/components/BannerStatus.js
+++ src/components/BannerStatus.js
@@ -5,6 +5,10 @@ import COLORS from '../styles/styleValues';
 const StyledBannerStatus = styled.div`
     background-color:${COLORS.accentMain};
     color:${COLORS.neutralBlack};
+    font-weight:700;
+    font-size: 1.2rem;
+    text-align:center;
+    padding:5px 10px;
 `;
 
 const BannerStatus = props => {
