0eb984f0278e173c223e924fe573adf57151053c
Suzanne Cabral <suzannecabral@users.noreply.github.com>
2021-05-06 17:27:26 -0700

    Fix searchContainer styles for deploy

--- src/components/SearchContainer.js
+++ src/components/SearchContainer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import COLORS from '../styles/styleValues';
+// import COLORS from '../styles/styleValues';
 
 import SearchResults from './SearchResults';
 import SelectedList from './SelectedList';
